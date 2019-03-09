c4dfc508435853e31ea5f60f107d9ec89633e584
Tommy Pettersson <tommypet@kth.se>
2019-03-09 16:41:02 +0100

    Enabled React.lazy to code split

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense} from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
-import AsyncComponent from './components/AsyncComponent';
+const Page2 = React.lazy(() => import('./components/Page2'));
+const Page3 = React.lazy(() => import('./components/Page3'));
 
 class App extends Component {
   constructor(){
@@ -14,46 +15,28 @@ class App extends Component {
   }
 
   onRouteChange = (route) =>{
-    //No code splitting:
     this.setState({route: route})
-    //With code splitting
-    // if(route === "page1"){
-    //   this.setState({route : route})
-    // }
-    // else if (route === "page2"){
-    //   import("./components/Page2")
-    //     .then((Page2) =>{
-    //       this.setState({ route: route, component: Page2.default})
-    //     })
-    // }
-    // else if(route === "page3")
-    // {
-    //   import("./components/Page3")
-    //   .then((Page3) =>{
-    //     this.setState({ route: route, component: Page3.default})
-    //   })
-    // }
   }
 
   render() {
     const {route} = this.state;
-    if(route === 'page1')
+
+    if(route === "page1")
       return <Page1 onRouteChange={this.onRouteChange}/>
-    else if (route === 'page2'){
-      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'))
-      return <AsyncPage2 onRouteChange={this.onRouteChange}/>
+    else if (route === "page2"){
+      return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Page2 onRouteChange={this.onRouteChange}/>
+          </Suspense>
+      )
     }
     else if (route === "page3"){
-      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'))
-      return <AsyncPage3 onRouteChange={this.onRouteChange}/>
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page3 onRouteChange={this.onRouteChange}/>
+        </Suspense>
+      )
     }
-      
-  //   if(route === "page1"){
-  //     return <Page1 onRouteChange={this.onRouteChange} />
-  //   } else {
-  //     return <this.state.component onRouteChange={this.onRouteChange} />
-  //   }
-    
   }
 }
 
